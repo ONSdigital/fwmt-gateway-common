61088154b2d9e8ac810a30a7b19ff6113392a83f
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2018-08-03 13:12:15 +0100

    initial project setup

---
+++ src/main/java/uk/gov/ons/fwmt/fwmtgatewaycommon/FwmtGatewayCommonApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.ons.fwmt.fwmtgatewaycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwmtGatewayCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwmtGatewayCommonApplication.class, args);
+	}
+}
