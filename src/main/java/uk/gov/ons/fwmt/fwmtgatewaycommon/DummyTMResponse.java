102b9fb479cafb1a5ac19e1533eba62659a1ae9a
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2018-08-09 16:48:51 +0100

    Added dummyTMResponse object

---
+++ src/main/java/uk/gov/ons/fwmt/fwmtgatewaycommon/DummyTMResponse.java
@@ -0,0 +1,12 @@
+package uk.gov.ons.fwmt.fwmtgatewaycommon;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DummyTMResponse {
+  private String identity;
+}
